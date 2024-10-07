e703054f0ffc042672baf9d3adc1a377e926e8c3
AlexsCruz151 <alexscruz821@gmail.com>
2024-10-07 16:36:21 -0600

    Usando ViewSet

--- apps/catalogos/departamentos/urls.py
+++ apps/catalogos/departamentos/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path
-from .views import DepartamentoApiView
+# from django.urls import path
+# from .views import DepartamentoApiView
+#
+# app_name = "departamentos"
+#
+# urlpatterns = [
+#     #path('', DepartamentoApiView.as_view(), name="departamento"),
+#     path('<str:pk>/', DepartamentoApiView.as_view()),
+# ]
 
-app_name = "departamentos"
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DepartamentoViewSet
+
+router = DefaultRouter()
+router.register(r'departamentos', DepartamentoViewSet, basename='departamento')
 
 urlpatterns = [
-    path('', DepartamentoApiView.as_view(), name="departamento"),
-    path('<str:pk>/', DepartamentoApiView.as_view()),
+    path('', include(router.urls)),
 ]
--- apps/catalogos/departamentos/views.py
+++ apps/catalogos/departamentos/views.py
@@ -1,113 +1,114 @@
-from django.core.serializers import serialize
-from django.shortcuts import render
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Departamento
+# from .serializers import DepartamentoSerializer
+#
+# class DepartamentoApiView(APIView):
+#
+#     def get(self, request, pk=None):
+#         # Si se proporciona el pk, obtenemos un solo departamento, de lo contrario, todos los departamentos
+#         if pk:
+#             try:
+#                 departamento = Departamento.objects.get(pk=pk)
+#             except Departamento.DoesNotExist:
+#                 return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
+#             serialize = DepartamentoSerializer(departamento)
+#             return Response(status=status.HTTP_200_OK, data=serialize.data)
+#         else:
+#             departamentos = Departamento.objects.all()
+#             serialize = DepartamentoSerializer(departamentos, many=True)
+#             return Response(status=status.HTTP_200_OK, data=serialize.data)
+#
+#     def post(self, request):
+#         serialize = DepartamentoSerializer(data=request.data)
+#         # Validar los datos enviados en el request
+#         if serialize.is_valid(raise_exception=True):
+#             serialize.save()  # Guardar el nuevo departamento
+#             return Response(status=status.HTTP_201_CREATED, data=serialize.data)
+#         return Response(status=status.HTTP_400_BAD_REQUEST, data=serialize.errors)
+#
+#     def put(self, request, pk):
+#         try:
+#             departamento = Departamento.objects.get(pk=pk)
+#         except Departamento.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
+#
+#         # Actualizar los datos del departamento
+#         serialize = DepartamentoSerializer(departamento, data=request.data)
+#         if serialize.is_valid(raise_exception=True):
+#             serialize.save()
+#             return Response(status=status.HTTP_200_OK, data=serialize.data)
+#         return Response(status=status.HTTP_400_BAD_REQUEST, data=serialize.errors)
+#
+#     def patch(self, request, pk):
+#         try:
+#             departamento = Departamento.objects.get(pk=pk)
+#         except Departamento.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
+#
+#         # Actualización parcial, solo actualiza los campos proporcionados
+#         serialize = DepartamentoSerializer(departamento, data=request.data, partial=True)
+#         if serialize.is_valid(raise_exception=True):
+#             serialize.save()
+#             return Response(status=status.HTTP_200_OK, data=serialize.data)
+#         return Response(status=status.HTTP_400_BAD_REQUEST, data=serialize.errors)
+#
+#     def delete(self, request, pk):
+#         try:
+#             departamento = Departamento.objects.get(pk=pk)
+#         except Departamento.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
+#
+#         departamento.delete()  # Eliminar el departamento
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework import status
+from .models import Departamento
 from .serializers import DepartamentoSerializer
-from django.shortcuts import get_object_or_404
 
-from apps.catalogos import departamentos
-from apps.catalogos.departamentos.models import Departamento
+class DepartamentoViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing, creating, updating, and deleting Departamento instances.
+    """
+    queryset = Departamento.objects.all()  # Define el conjunto de datos
+    serializer_class = DepartamentoSerializer  # Usa el serializador para los datos
 
+    # Sobrescribir el método create (POST)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-class DepartamentoApiView(APIView):
-    # def get1(self, request):
-    #   #departamentos = [de.codigo for de in Departamento.objects.all()]
-    #   departamentos = list(Departamento.objects.values())
-    #   return Response(status=status.HTTP_200_OK,data=departamentos)
-    #   #return Response(status=status.HTTP_200_OK,data={'departamentos': Departamento.objects.all()})
+    # Sobrescribir el método retrieve (GET por id)
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-    def get(self, request):
-        serialize = DepartamentoSerializer(Departamento.objects.all(),many=True)
-        return Response(status=status.HTTP_200_OK,data=serialize.data)
+    # Sobrescribir el método update (PUT)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
 
-    # def post1(self, request):
-    #     # departamento = Departamento()
-    #     # departamento.codigo = request.data['codigo']
-    #     # departamento.nombre = request.data['nombre']
-    #     # departamento.save()
-    #     Departamento.objects.create(codigo=request.data['codigo'], nombre=request.data['nombre'])
-    #     return Response(status=status.HTTP_201_CREATED)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
 
-    def post(self, request):
-        serialize=DepartamentoSerializer(data=request.data)
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_201_CREATED,data=serialize.data)
+        return Response(serializer.data)
 
-    def put(self, request, pk):
-        try:
-            departamento = Departamento.objects.get(pk=pk)
-        except Departamento.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
+    # Sobrescribir el método partial_update (PATCH)
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
 
-        serialize = DepartamentoSerializer(departamento, data=request.data)
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_200_OK, data=serialize.data)
-
-    def delete(self, request, pk):
-        try:
-            departamento = Departamento.objects.get(pk=pk)
-        except Departamento.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
-
-        departamento.delete()
+    # Sobrescribir el método destroy (DELETE)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def patch(self, request, pk):
-        try:
-            departamento = Departamento.objects.get(pk=pk)
-        except Departamento.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Departamento no encontrado"})
-
-        serialize = DepartamentoSerializer(departamento, data=request.data, partial=True)  # Permite actualizar solo los campos proporcionados
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_200_OK, data=serialize.data)
-
-
-class DepartamentoApiView2(APIView):
-    # Obtener lista de departamentos o filtrar por nombre
-    def get(self, request):
-        # Filtrar por nombre si se proporciona en los parámetros de consulta
-        nombre = request.query_params.get('nombre', None)
-        if nombre:
-            departamentos = Departamento.objects.filter(nombre__icontains=nombre)  # Filtrar por nombre (contiene)
-        else:
-            departamentos = Departamento.objects.all()  # Obtener todos los departamentos
-
-        serialize = DepartamentoSerializer(departamentos, many=True)
-        return Response(status=status.HTTP_200_OK, data=serialize.data)
-
-    # Crear un nuevo departamento
-    def post(self, request):
-        serialize = DepartamentoSerializer(data=request.data)
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_201_CREATED, data=serialize.data)
-
-    # Actualizar completamente un departamento (PUT)
-    def put(self, request, pk):
-        departamento = get_object_or_404(Departamento, pk=pk)  # Buscar el departamento por ID
-        serialize = DepartamentoSerializer(departamento, data=request.data)
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_200_OK, data=serialize.data)
-
-    # Actualizar parcialmente un departamento (PATCH)
-    def patch(self, request, pk):
-        departamento = get_object_or_404(Departamento, pk=pk)  # Buscar el departamento por ID
-        serialize = DepartamentoSerializer(departamento, data=request.data, partial=True)  # Permitir actualización parcial
-        serialize.is_valid(raise_exception=True)
-        serialize.save()
-        return Response(status=status.HTTP_200_OK, data=serialize.data)
-
-    # Eliminar un departamento
-    def delete(self, request, pk):
-        departamento = get_object_or_404(Departamento, pk=pk)  # Buscar el departamento por ID
-        departamento.delete()  # Eliminar el departamento
-        return Response(status=status.HTTP_204_NO_CONTENT)  # No se devuelve contenido en la respuesta
-
-
-
